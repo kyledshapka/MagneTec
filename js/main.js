6c3bf80614be016688ae9fdcf993d32cb84a4d90
kyledshapka <kyledshapka@gmail.com>
2017-05-08 01:06:24 -0600

    Add magnet generation from test API and location updating to server
    Magnets are now dynamically generated from a test RESTful API courtesy of http://rest.learncode.academy. In addition, when magnets are dragged they now update their data on the server accordingly. As the data on the server is cleared nightly, the test.json file is still useful for initializing a data set for use in testing.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 $(function() {
     var $door = $('#door');
+    var $root = "http://rest.learncode.academy/api/magneTec/magnet"; // http://vfm.sigrd.com or https://vfm.sigrd.com
+    // var $poot = "test/test.json"; // this is just initializing some magnets for testing.
 
     /* Create a div of class "magnet" from an input magnet object which includes: id, word, x and y location.
     * Magnet divs are draggable and include an event listener for when they are dropped.
     * Finally, add the magnet to the #door div, which serves as our canvas and represents the fridge door. */
     function addMagnet(magnet){
-        // Actual format unknown, must be updated once more API details are made available. !!!!!!!!!!!!!!!!!!!!!!!
         var $magnet = $('<div id = ' + magnet.id + ' class = "magnet">' + magnet.word + '</div>');
-        var y = magnet.y;
-        var x = magnet.x;
-        $magnet.css({top: y, left: x, position: 'absolute'});
+        var $y = parseInt(magnet.y);
+        var $x = parseInt(magnet.x);
+        $magnet.css({id: magnet.id, top: $y, left: $x, position: 'absolute'});
 
         // Make the magnet into a draggable object and give it an event listener for being dropped.
         $magnet.draggable({
-            stop: function( event, ui, index ) {},
+            stop: function( event, ui) {},
             containment: "parent",
             scroll: false,
             stack: ".magnet"
@@ -25,28 +26,45 @@ $(function() {
     // Ask the API for a list of the magnets with their words and locations.
     $.ajax({
             type: 'GET',
-            url: 'test/test.json',
+            url: $root,
             success: function(magnets) {
                 // Add all of the magnets to the fridge door.
                 $.each(magnets, function(index, magnet) {
                     addMagnet(magnet);
                 });
 
-                // Record the magnet in the console for testing purposes.
+                // Record the magnet as an object appropriate for posting to the API
                 $('.magnet').on("dragstop", function(event, ui){
                     var magnet = $ ( this );
-                    var word = magnet.text();
-                    var x = magnet.position().left;
-                    var y = magnet.position().top;
-                    console.log( word + "| x: " + x + ", y: " + y );
+                    var $id = magnet.attr("id");
+                    var $word = magnet.text();
+                    var xLoc = magnet.position().left;
+                    var yLoc = magnet.position().top;
+                    // The completed object
+                    var magnetObject = {
+                        word: $word,
+                        x: xLoc,
+                        y: yLoc,
+                        id: $id
+                    };
+
+                    // Post that magnet data to the API
+                    $.ajax({
+                        type: 'PUT',
+                        data: magnetObject,
+                        url: $root + "/" + $id,
+                        success: function(){
+                            console.log( $word + " | x: " + xLoc + ", y: " + yLoc ); // TESTING
+                        },
+                        error: function(){
+                            alert('error moving magnet on server')
+                        }
+                    });
                 });
             },
             error: function(){
                 alert('error getting magnets');
             }
-    });
-    $('.magnet').each(function(index) {
-        console.log(index + ": " + $(this).text()); // for testing purposes
     });
 });
 
