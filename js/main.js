ac6e58ec0c5937090ae72024e39fe4ff4926e9a4
kyledshapka <kyledshapka@gmail.com>
2017-05-10 01:34:50 -0600

    Enable GET request from real API
    Magnets are now generated from the server and periodically updated.
    When magnets are drag n'dropped, they now attempt a PUT request to
    update the server with their new location... however, getting an error
    response through AJAX. See issue #4.
    Renamed fridge.html to index.html and fridgestyle.css to fridge.css.
    Big code cleanup performed in main.js.

--- js/main.js
+++ js/main.js
@@ -1,92 +1,127 @@
-$(function() {
-    // The URL of the server which will store the magnets
-    var $root = "http://rest.learncode.academy/api/magneTec/magnet"; // http://vfm.sigrd.com or https://vfm.sigrd.com
-    var $poot = "test/test.json"; // Useful for initializing some magnets during testing when server has been cleared.
+$( function() {
+    var apiUrl = "http://vfm.sigrd.com/api/fridges";
+    var fridgeName = "alpha";
+    var fridgeUrl = apiUrl + "/" + fridgeName;
 
-    // Populate the local door from the API.
-    $(function(){
-        getMagnets();
-    });
+    // Get the magnets from the named fridge
+    getMagnets ( fridgeUrl, fridgeName );
+});
 
-    // Displays the list of words and their locations when the poll button is clicked.
-    $("#pollButton").click(function(){
-        var result = [];
-        $.get($root, function(data){
-            $.each(data, function(index, magnet){
-                var $word = magnet.word;
-                var $y = magnet.y;
-                var $x = magnet.x;
-                var $id = magnet.id;
-                var $entry = ("(" + $x + ", " + $y + ") : " + $word + "\n\n");
-                result.push($entry);
+/*************
+ * Functions *
+ ************/
+
+    /******************************************************************************************************
+     * getMagnets: periodically updates the local version of magnets found on the fridge at the input URL *
+     *****************************************************************************************************/
+    function getMagnets( fridgeUrl, fridgeName ) {
+        var wordsUrl = fridgeUrl + "/words";
+        var updateInterval = 2000; // How often the magnet locations should be checked.
+
+        setInterval( function () {
+            $( "#door" ).empty();
+            $.ajax({
+                type: 'GET',
+                url: wordsUrl,
+                success: function( fridge ){
+
+                    var words = fridge.words;
+                    drawWords( words, fridgeUrl );
+
+                },
+                // Alert the user if the fridge cannot be reached
+                error: function(){
+                    alert( "Error accessing " + fridgeName + " at " + apiUrl )
+                }
             });
-            alert("The following are stored on the server along with their (x,y) locations:\n\n" + result.join(""));
-        });
-    });
+        }, updateInterval);
+
+    }
 
 
-    /******************************************************************************************************************
-     * Requests the list of magnets from the server and places them in the appropriate location on the "fridge door". *
-     *****************************************************************************************************************/
-    timer = setInterval( getMagnets, 5000);
-    function getMagnets(){
-        $("#door").empty();
-        $.get($root, function(data){
-            $.each(data, function(index, magnet){
-                var $magnet = $('<div id = ' + magnet.id + ' class = "magnet">' + magnet.word + '</div>');
-                var $y = parseInt(magnet.y);
-                var $x = parseInt(magnet.x);
-                $magnet.css({id: magnet.id, top: $y, left: $x, position: 'absolute'});
+    /***************************************************************************************************
+    * drawWords: takes a wordsObject and creates matching magnets on the canvas.                       *
+    * The words can be drag n'dropped, and update their location to the server when moved by the user. *
+    *                                                                                                  *
+    *  A wordsObject contains all the information about the words stored on this fridge:               *
+    *                                                                                                  *
+    *    .update_at = when the list was last updated                                                   *
+    *    .total = the total number of words on this fridge                                             *
+    *    .fridge = an array with the .name and .href of the fridge where these words are stored.       *
+    *    .list = the list of words in an array, each of which has:                                     *
+    *                                                                                                  *
+    *      [.id = this word's id number                                                                *
+    *      .txt = the text of this word                                                                *
+    *      .x = the x location of this word                                                            *
+    *      .y = the y location of this word]                                                           *
+    ***************************************************************************************************/
+    function drawWords ( wordsObject, fridgeUrl ){
+        $.each( wordsObject.list, function( index, word ){
+            // Using the word data, create a draggable magnet and give it an event listener for being dropped.
+            makeDraggableDiv ( word, fridgeUrl );
 
-                // Make the magnet into a draggable object and give it an event listener for being dropped.
-                $magnet.draggable({
-                    start: function(event, ui) {},
-                    stop: function( event, ui ) {},
-                    containment: "parent",
-                    scroll: false,
-                    stack: ".magnet"
-                });
-                $magnet.on("dragstart", function(event, ui) {
-                    clearInterval(timer);
-                });
-                $magnet.on("dragstop", function( event, ui) {
-                    timer = setInterval( getMagnets, 5000);
-                    sendMagnet($magnet);
-                });
-                console.log($magnet); // for testing
-                $("#door").append($magnet);
-            });
+            // Add the magnet to the fridge's canvas
+                $( "#door" ).append( magnet );
         });
     }
 
 
-    /*******************************************************************
-     * Updates the server copy of a magnet from the local information. *
-     ******************************************************************/
-    function sendMagnet(magnet) {
-        var $id = magnet.attr("id");
-        var $word = magnet.text();
-        var xLoc = magnet.position().left;
-        var yLoc = magnet.position().top;
-        // The completed object
-        var magnetObject = {
-            word: $word,
-            x: xLoc,
-            y: yLoc,
-            id: $id
-        };
+    /***********************************************************************************
+    * Makes a draggable div object out of the input word's id, text, and x,y location. *
+    ***********************************************************************************/
+    function makeDraggableDiv ( word, fridgeUrl ) {
+        var $id = word.id,
+            $text = word.txt,
+            $x = word.x,
+            $y = word.y;
 
-        // Post that magnet data to the API
+        // Translate the word objects into html divs for display on the page
+        magnet = $( '<div id = ' + $id + ' class = "magnet">' + $text + '</div>' );
+        magnet.css( {id: $id, top: $y, left: $x, position: 'absolute'} );
+
+        // Enable drag n'drop
+        magnet.draggable({
+            containment: "parent",
+            scroll: false,
+            stack: ".magnet"
+        });
+
+        // Pause updating locally while a magnet is being dragged
+        magnet.on( "dragstart", function() {
+            // DO SOMETHING (future implementation - pause updating while this magnet is being dragged)
+        });
+
+        // Update the server after a magnet has been moved
+        magnet.on( "dragstop", function() {
+
+            sendMagnet( $(this), fridgeUrl );
+        });
+    }
+
+
+    /************************************************************
+    * Updates the server with the new location of the magnet.   *
+    * The magnet must be sent in the form: { "x":234, "y":345 } *
+    * to http://vfm.sigrd.com/api/fridges/word/<wordID>         *
+    ************************************************************/
+    function sendMagnet( magnet, fridgeUrl ) {
+        var $id = magnet.attr("id"),
+            $text = magnet.text(),
+            $x = magnet.position().left,
+            $y = magnet.position().top;
+        var magnetObject = { "x":$x, "y":$y };
+        var magnetUrl = fridgeUrl + "/word/" + $id;
+        console.log( fridgeUrl + "\n" + $id + " | " + $text + " @ " + $x + "," + $y ); // TEST
+
         $.ajax({
             type: 'PUT',
             data: magnetObject,
-            url: $root + "/" + $id,
+            url: magnetUrl,
             success: function(){
-                console.log( $word + " | x: " + xLoc + ", y: " + yLoc ); // TESTING
+
             },
             error: function(){
                 alert('error moving magnet on server')
             }
         });
     }
-});
